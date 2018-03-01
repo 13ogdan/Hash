a784c9a29b8e28cb7c8dc16e329fbc7f148d0b2a
Artem Ripko <rtemka@gmail.com>
2018-03-01 23:10:05 +0200

    res

--- GoogleHash/SelfDrivingRides/Program.cs
+++ GoogleHash/SelfDrivingRides/Program.cs
@@ -18,6 +18,31 @@ namespace SelfDrivingRides
             public int FinishCol { get; set; }
             public int StartT { get; set; }
             public int FinishT { get; set; }
+            
+            public Ride BestNext { get; set; }
+
+            public void IsNext(Ride r)
+            {
+                // if possible
+                if (r.StartT < StartT + Length())
+                    return;
+
+                if (!(StartT + Length() + r.DistToStart(FinishRow, FinishCol) < r.FinishT - r.Length()))
+                {
+                    return;
+                }
+
+                if (BestNext == null)
+                {
+                    BestNext = r;
+                    return;
+                }
+
+                if (r.DistToStart(FinishRow, FinishCol) < BestNext.DistToStart(FinishRow, FinishCol))
+                {
+                    BestNext = r;
+                }
+            }
 
             public int Score(int startTime, int bonus)
             {
@@ -50,6 +75,11 @@ namespace SelfDrivingRides
                     return 1;
                 return 0;
             }
+
+            public override string ToString()
+            {
+                return N.ToString();
+            }
         }
 
         public class Problem
@@ -70,6 +100,23 @@ namespace SelfDrivingRides
             public long Score { get; set; }
             public int CurRow { get; set; }
             public int CurCol { get; set; }
+            public long CurT { get; set; }
+            
+            public List<int> RidesProcessing { get; } = new List<int>();
+
+            public void ApplyRide(Ride r)
+            {
+                var d = r.DistToStart(CurRow, CurRow);
+                CurT += d;
+                if (r.StartT < CurT)
+                    CurT = r.StartT;
+
+                CurT += r.Length();
+                CurRow = r.FinishRow;
+                CurCol = r.FinishCol;
+
+                Rides.Add(r);
+            }
         }
 
 
@@ -104,7 +151,7 @@ namespace SelfDrivingRides
             return res;
         }
 
-        private static void WriteOutput(List<Vehicle> res, string filename)
+        private static void WriteOutput(List<Vehicle> res, string filename, Problem problem)
         {
             if (File.Exists(filename))
                 File.Delete(filename);
@@ -114,27 +161,101 @@ namespace SelfDrivingRides
                 var rides = string.Join(" ", v.Rides.Select(r => r.N.ToString()));
                 File.AppendAllText(filename, string.Format($"{v.Rides.Count} {rides}\n"));
             }
+
+            if (res.Count < problem.Fleet)
+            {
+                for (int i = 0; i < problem.Fleet-res.Count; i++)
+                {
+                    File.AppendAllText(filename, string.Format($"0\n"));
+                }
+            }
+        }
+        
+        private static long CalcRideSeq(Ride r, int bonus)
+        {
+            var startT = r.StartRow + r.StartCol;
+            if (startT + r.Length() > r.FinishT)
+                return 0;
+
+            long score = r.Score(startT, bonus); 
+            var cur = r;
+            while (cur.BestNext!=null)
+            {
+                if (startT <= cur.StartT)
+                    startT = cur.StartT;
+                startT += cur.Length();
+
+                cur = cur.BestNext;
+
+                if (startT + cur.Length() > cur.FinishT)
+                    return score;
+
+                score += cur.Score(startT, bonus);
+            }
+            return score;
         }
 
+        private static List<Vehicle> Algo(Problem problem)
+        {
+            var res = new List<Vehicle>();
+
+            var ridesToGo = new List<Ride>();
+            foreach (var r in problem.Rides)
+            {
+                ridesToGo.Add(r);
+            }
+
+            var prevC = ridesToGo.Count + 1;
+            while (ridesToGo.Count > 0 && ridesToGo.Count != prevC && res.Count < problem.Fleet)
+            {
+                prevC = ridesToGo.Count;
+                foreach (var r1 in ridesToGo)
+                {
+                    r1.BestNext = null;
+                    foreach (var r2 in ridesToGo)
+                    {
+                        if (r1 != r2)
+                            r1.IsNext(r2);
+                    }
+                }
+
+                long bestProf = 0;
+                Ride start = null;
+                foreach (var r in ridesToGo)
+                {
+                    var prof = CalcRideSeq(r, problem.Bonus);
+                    if (prof > bestProf)
+                    {
+                        bestProf = prof;
+                        start = r;
+                    }
+                }
+
+                if (start != null)
+                {
+                    var v = new Vehicle();
+                    v.Rides.Add(start);
+                    ridesToGo.Remove(start);
+                    var cur = start;
+                    while (cur.BestNext != null)
+                    {
+                        cur = cur.BestNext;
+                        v.Rides.Add(cur);
+                        ridesToGo.Remove(cur);
+                    }
+                    res.Add(v);
+                }
+            }
+
+            return res;
+        }
 
 
         static void Main(string[] args)
         {
             var problem = ReadInput(args[0]);
-            var res = new List<Vehicle>();
-            for (int i = 0; i < problem.Fleet; i++)
-            {
-                res.Add(new Vehicle
-                {
-                    CurCol = 0,
-                    CurRow = 0,
-                    N = i
-                });
-            }
-
-
-
-            WriteOutput(res, "output.out");
+            var res = Algo(problem);
+            WriteOutput(res, "output.out",problem);
         }
     }
 }
